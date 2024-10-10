bdc4d855339edac17691eaccae565c494007aec1
renandaadesantoso <santosoade37961@gmail.com>
2024-10-10 21:16:58 +0700

    Update 8-A.cpp

---
+++ Tugas TLX/2405100109_RENANDA ADE SANTOSO/8-A.cpp
@@ -0,0 +1,23 @@
+Pak Dengklek memiliki sebuah kandang berbentuk persegi panjang yang berukuran:
+
+panjang 
+2002419288
+2002419288 nanometer
+lebar 
+1040323716
+1040323716 nanometer
+Cetaklah luasnya, dalam nanometer persegi!
+
+  #include <iostream>
+using namespace std;
+
+int main() {
+    long long panjang = 2002419288;
+    long long tinggi = 1040323716;
+
+    // Pastikan ekspresi perkalian menggunakan tipe long long
+    long long luas = panjang * tinggi;
+    cout << luas << endl;
+
+    return 0;
+}
